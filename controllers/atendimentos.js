35bfb151dfe93becb2eb70e5bb69c049e4aab65c
Thais Helena Moretto <CIANDT\thaism@lnb028293cps.ciandt.global>
2021-03-24 17:06:26 -0300

    Conclusao do projeto

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,12 +1,37 @@
 // Responsabilidade do atendimentos.js = controlar as rotas existentes e o que deve ser feito quando acessar cada uma delas
 
+const atendimentos = require('../models/atendimentos')
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     // Envio de dados para o client:
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.buscaPorId(id, res)
+    })
 
     // 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
-        res.send('Você está na rota de atendimentos e está realizando um POST')
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento, res)
+    })
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.delete(id, res)
     })
 }
